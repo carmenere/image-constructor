d1ffa66ee7f0b147c8832542dbd7816d4b20dae9
Anton Romanov <theblackswan.msk@gmail.com>
2018-08-20 14:02:25 +0300

    Some fixes.

--- debootstrap.sh
+++ debootstrap.sh
@@ -11,12 +11,11 @@ continue_with_defaults () {
 }
 
 
-
 prepare_apt_source_list () {
 case $OS in
     "ubuntu")
-        truncate -s 0 images/${OS}/${CODENAME}/etc/apt/sources.list
-        cat <<EOF > images/${OS}/${CODENAME}/etc/apt/sources.list
+        truncate -s 0 /tmp/${OS}/${CODENAME}/etc/apt/sources.list
+        cat <<EOF > /tmp/${OS}/${CODENAME}/etc/apt/sources.list
 ###### Ubuntu Main Repos
 deb ${APT_URI} ${CODENAME} main restricted universe multiverse
 deb-src ${APT_URI} ${CODENAME} main restricted universe multiverse
@@ -30,8 +29,8 @@ deb-src ${APT_SECURITY_URI} ${CODENAME}-security main restricted universe multiv
 EOF
         ;;
     "debian")
-        truncate -s 0 images/${OS}/${CODENAME}/etc/apt/sources.list
-        cat <<EOF > images/${OS}/${CODENAME}/etc/apt/sources.list
+        truncate -s 0 /tmp/${OS}/${CODENAME}/etc/apt/sources.list
+        cat <<EOF > /tmp/${OS}/${CODENAME}/etc/apt/sources.list
 deb ${APT_URI} ${CODENAME} main contrib non-free
 deb-src ${APT_URI} ${CODENAME} main contrib non-free
 
@@ -47,18 +46,20 @@ esac
 
 if [[ $# -eq 0 ]]
 then
-    echo "No arguments supplied!"
-    echo "Usage: ${0##*/} OS release_codename base_image_tag [derivated_image_tag]"
+    echo "ERROR: No arguments supplied!"
+    echo ""
+    echo "Usage: ${0##*/} OS release_codename base_image_tag"
     continue_with_defaults "OS=ubuntu, release_codename=xenial, tag=ubuntu/xenial:16.04"
     OS="ubuntu"
     CODENAME="xenial"
     TAG="ubuntu/xenial:16.04"
     URI="http://archive.ubuntu.com/ubuntu/"
     APT_URI="http://ru.archive.ubuntu.com/ubuntu/"
-elif [[ $# -gt 4 ]] || [[ $# -lt 3 ]]
+elif [[ $# -ne 3 ]]
 then
-    echo "Incorrect arguments value supplied!"
-    echo "Usage: ${0##*/} OS release_codename base_image_tag [derivated_image_tag]"
+    echo "ERROR: Incorrect arguments value supplied!"
+    echo ""
+    echo "Usage: ${0##*/} OS release_codename base_image_tag"
     continue_with_defaults "OS=ubuntu, release_codename=xenial, tag=ubuntu/xenial:16.04"
     OS="ubuntu"
     CODENAME="xenial"
@@ -83,7 +84,9 @@ else
         ;;
 
     *)
-        echo "Argument OS has incorrect vlue!"
+        echo ""
+        echo "ERROR: Argument OS has incorrect value!"
+        echo ""
         continue_with_defaults "OS=ubuntu"
         OS="ubuntu"
         URI="http://archive.ubuntu.com/ubuntu/"
@@ -108,17 +111,16 @@ else
   esac
 
   if [[ -n "$3" ]]; then
-    TAG=$3
+      TAG=$3
   else
-  	TAG="ubuntu/xenial:16.04"
+      TAG="ubuntu/xenial:16.04"
   fi
 
   if [[ -n "$4" ]]; then
-    TAG2=$4
+      TAG2=$4
   fi
 
 fi
-
 
 if [[ -z "${APT_SECURITY_URI}" ]]; then
 	APT_SECURITY_URI="${APT_URI}"
@@ -134,11 +136,11 @@ select YN in "Yes" "No"; do
 done
 
 
-if [[ -e images/${OS}/${CODENAME} ]]; then
-  rm -rf images/${OS}/${CODENAME}
+if [[ -e /tmp/${OS}/${CODENAME} ]]; then
+  rm -rf /tmp/${OS}/${CODENAME}
 fi
 
-mkdir -p images/${OS}/${CODENAME}
+mkdir -p /tmp/${OS}/${CODENAME}
 
 
 debootstrap --help > /dev/null 2>&1
@@ -149,9 +151,9 @@ then
 fi
 
 
-debootstrap --arch=amd64 --variant=minbase --include=bash-completion,nano,sudo,nmap,zlib1g-dev,libssl-dev,zip,unzip,git,strace,ltrace,curl,build-essential,conntrack,ipset,tcpdump,openssh-server ${CODENAME} images/${OS}/${CODENAME} ${URI}
-#debootstrap --arch=amd64 --include=bash-completion,nano,sudo,nmap,zlib1g-dev,libssl-dev,zip,unzip,git,strace,ltrace,curl ${CODENAME} images/${OS}/${CODENAME} ${URI}
-#debootstrap --arch=amd64 ${CODENAME} images/${OS}/${CODENAME} ${URI}
+debootstrap --arch=amd64 --variant=minbase ${CODENAME} /tmp/${OS}/${CODENAME} ${URI}
+#debootstrap --arch=amd64 --variant=minbase --include=bash-completion,nano,sudo,nmap,zlib1g-dev,libssl-dev,zip,unzip,git,strace,ltrace,curl,build-essential,conntrack,ipset,tcpdump,openssh-server ${CODENAME} /tmp/${OS}/${CODENAME} ${URI}
+
 
 if [[ $? -ne 0 ]]
 then
@@ -163,50 +165,8 @@ fi
 prepare_apt_source_list
 
 
-tar -C images/${OS}/${CODENAME} -c . | docker import - ${TAG}
+tar -C /tmp/${OS}/${CODENAME} -c . | docker import - ${TAG}
 
 
-if [[ -z "${TAG2}" ]]; then
-	exit
-fi
+rm -rf /tmp/${OS}
 
-truncate -s 0 images/${OS}/${CODENAME}/Dockerfile
-cat <<EOF > images/${OS}/${CODENAME}/Dockerfile
-FROM ${TAG}
-
-ARG TERM=xterm-256color
-ARG DEBIAN_FRONTEND=noninteractive
-
-ENV TERM=xterm-256color
-
-RUN apt update && \
-    apt upgrade -y && \
-    apt clean
-
-RUN apt install -y scapy && \
-    apt install -y lshw && \
-    apt install -y htop && \
-    apt install -y tshark && \
-    apt install -y tree && \
-    apt install -y python3-dev && \
-    apt install -y python3-yaml && \
-    apt install -y python3-pip && \
-    apt install -y python3-setuptools && \
-    apt install -y python3-virtualenv && \
-    apt install -y python3-numpy && \
-    apt install -y python3-scipy && \
-    apt install -y python3-matplotlib && \
-    apt install -y python3-pandas && \
-    apt install -y ipython3 && \
-    pip3 install --upgrade pip setuptools && \
-    apt install -y nginx && \
-    apt install -y postgresql
-
-LABEL OS="ubuntu/xenial:16.04"
-
-ENTRYPOINT ["/bin/bash"]
-
-EOF
-
-
-docker build -f images/${OS}/${CODENAME}/Dockerfile images/${OS}/${CODENAME} --tag "${TAG2}"
